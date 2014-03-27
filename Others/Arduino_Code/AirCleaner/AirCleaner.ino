44e1417c9a2a5678ffaa9de142f30e31189a0dd7
DuanWeiye <duan.weiye@gmail.com>
2014-03-27 13:08:31 +0800

    First

--- Others/Arduino_Code/AirCleaner/AirCleaner.ino
+++ Others/Arduino_Code/AirCleaner/AirCleaner.ino
@@ -2,6 +2,7 @@
 #include <Library_LCD.h>
 #include <dht11.h>
 
+#define DSM501A_WARMUP  2
 #define DHT_SENSOR_ERR  -255
 #define FAN_SPEED_MAX   249
 #define FAN_SPEED_MIN   0
@@ -11,6 +12,7 @@
 #define PIN_LED_BLUE    5
 #define PIN_FAN         3
 #define PIN_DHT         4
+#define PIN_DSM501A     10
 
 dht11 dhtSensor;
 Library_LCD lcd;
@@ -22,6 +24,15 @@ int led_b = 0;
 
 int hum = 0;
 int tem = 0;
+int pm = 0; // pcs/0.01cf
+int pm_warmup_count = 0;
+
+unsigned long duration;
+unsigned long starttime;
+unsigned long endtime;
+unsigned long sampletime_ms = 30000;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
 
 void setup()
 {
@@ -36,104 +47,115 @@ void setup()
   ControlFan(0);
   
   pinMode(PIN_FAN, OUTPUT);
-  
-  lcd.ClearLCD();
-  
-  LedFade(255,0,0,25);
-  LedFade(0,255,0,25);
-  LedFade(0,0,255,25);
+  pinMode(PIN_DSM501A,INPUT);
   
   DrawLCD();
+   
+  LedFade(255,0,0,15);
+  LedFade(0,255,0,15);
+  LedFade(0,0,255,15);
+  
+  starttime = millis();
 }
 
 void loop()
 {
   if (true == GetHumidityAndTemperature(&hum, &tem))
   {
-    lcd.LocateLCD(0, 4);
-    lcd.PrintLCD((String)hum + "/" + (String)tem);
+    lcd.LocateLCD(0, 0);
+    lcd.PrintLCD((String)hum + "%/" + (String)tem + "C");
   }
   else
   {
-    lcd.LocateLCD(0, 4);
-    lcd.PrintLCD("--/--");
+    lcd.LocateLCD(0, 0);
+    lcd.PrintLCD("---/---");
   }
   
-  if (hum == DHT_SENSOR_ERR || tem ==DHT_SENSOR_ERR)
+  if (true == GetDust() && pm != 0)
+  {
+    lcd.LocateLCD(1, 7);
+    lcd.PrintLCD((String)pm + "   ");
+  }
+  
+  if (pm == 0)
+  {
+    fanSpeed = 0;
+    led_r = 0;
+    led_g = 0;
+    led_b = 255;
+    
+    lcd.LocateLCD(0, 13);
+    lcd.PrintLCD("MIN");    
+  }
+  else if (pm > 0 && pm <= 200)
+  {
+    fanSpeed = 0;
+    led_r = 0;
+    led_g = 255;
+    led_b = 0;
+    
+    lcd.LocateLCD(0, 13);
+    lcd.PrintLCD("MIN");
+  }
+  else if (pm > 200 && pm <= 500)
+  {
+    fanSpeed = 50;
+    led_r = 255;
+    led_g = 255;
+    led_b = 0;
+    
+    lcd.LocateLCD(0, 13);
+    lcd.PrintLCD("MED");    
+  }
+  else
   {
     fanSpeed = 99;
     led_r = 255;
     led_g = 0;
     led_b = 0;
+    
+    lcd.LocateLCD(0, 13);
+    lcd.PrintLCD("MAX");
   }
-  else if (hum > 30 && hum < 40 && tem > 18 && tem < 32)
-  {
-    fanSpeed = 25;
-    led_r = 0;
-    led_g = 255;
-    led_b = 0;
-  }
-  /*
-  else
-  {
-    fanSpeed = 50;
-    led_r = hum < 50 ? hum * 255 / 50 : 255;
-    led_g = tem < 50 ? tem * 255 / 50 : 255;
-    led_b = tem < hum ? ( hum - tem > 10 ? 255 : ( hum - tem ) * 255 / 10 ) : ( tem - hum > 10 ? 255 : ( tem - hum ) * 255 / 10 );
-  }
-  */
-  else
-  {
-    fanSpeed = 50;
-    led_r = 255;
-    led_g = 255;
-    led_b = 0;
-  }
-
-  ControlFan(fanSpeed);  
-  lcd.LocateLCD(0, 14);
-  lcd.PrintLCD(fanSpeed);
-  lcd.LocateLCD(1, 5);
-  lcd.PrintLCD("           ");
-  lcd.LocateLCD(1, 5);
-  lcd.PrintLCD((String)led_r + "/" + (String)led_g + "/" + (String)led_b);
-  LedFade(led_r, led_g, led_b, 5);
+  
+  ControlFan(fanSpeed);
+  
+  LedFade(led_r, led_g, led_b, 1);
 }
 
 void DrawLCD()
 {
   // |0123456789ABCDEF|
   // |----------------|
-  //  H/T:xx/xx Fan:xx  
-  //  Col: 255/255/255
+  //  xx%/xxC  Fan:xxx  
+  //  PM2.5: xxx
   // |----------------|
   
   lcd.ClearLCD();
-  lcd.LocateLCD(0, 0);
-  lcd.PrintLCD("H/T:");
   
-  lcd.LocateLCD(0, 10);
-  lcd.PrintLCD("Fan:");
+  lcd.LocateLCD(0, 0);
+  lcd.PrintLCD("---/---");
+  
+  lcd.LocateLCD(0, 9);
+  lcd.PrintLCD("Fan:---");
   
   lcd.LocateLCD(1, 0);
-  lcd.PrintLCD("Col:");
+  lcd.PrintLCD("PM2.5: ---");
 }
 
 void LedFade(int R, int G, int B, int interval)
 {
   LedRGB(0,0,0);
   
-  for(int fadeValue = 0; fadeValue <= 255; fadeValue += interval) { 
+  for(int fadeValue = 0; fadeValue <= 255; fadeValue += interval) {
     LedRGB(R * fadeValue / 255, G * fadeValue / 255, B * fadeValue / 255);
-    delay(50);                            
+    delay(10);                            
   } 
-  delay(200);
   
   for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -= interval) { 
     LedRGB(R * fadeValue / 255, G * fadeValue / 255, B * fadeValue / 255);
-    delay(50);                          
+    delay(10);                          
   }
-  delay(200);
   
   LedRGB(0,0,0);
 }
@@ -177,6 +199,43 @@ bool ControlFan(int speedPercent)
   }
   else
   {
+    return false;
+  }
+}
+
+bool GetDust()
+{
+  duration = pulseIn(PIN_DSM501A, LOW);
+  lowpulseoccupancy += duration;
+  endtime = millis();
+  
+  if ((endtime - starttime) > sampletime_ms)
+  {
+    if (pm_warmup_count < DSM501A_WARMUP)
+    {
+      pm_warmup_count++;
+    }
+    else
+    {
+      ratio = (lowpulseoccupancy - endtime + starttime + sampletime_ms) / (sampletime_ms * 10.0);
+      pm = 1.1 * pow(ratio, 3) - 3.8 * pow(ratio, 2) + 520 * ratio + 0.62;
+      float offsetFix = (endtime - starttime - 30000) / 30000.0;
+      pm = (1.00 - offsetFix) * pm / ratio;
+  
+      //Serial.print(ratio);
+      //Serial.print(",");
+      //Serial.println(pm);
+      //Serial.println(endtime - starttime);
+    }
+    
+    lowpulseoccupancy = 0;
+    starttime = millis();
+    
+    return true;
+  }
+  else
+  {
+    //Serial.println(endtime - starttime);
     return false;
   }
 }
